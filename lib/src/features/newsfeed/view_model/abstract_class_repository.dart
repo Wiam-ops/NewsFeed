fcf5b0b844ad179cb282a19536db855a4e482c87
Wiam-ops <122654618+Wiam-ops@users.noreply.github.com>
2023-05-15 22:31:35 +0100

    Update abstract_class_repository.dart

--- lib/src/features/newsfeed/view_model/abstract_class_repository.dart
+++ lib/src/features/newsfeed/view_model/abstract_class_repository.dart
@@ -1,6 +1,1 @@
-import '../model/article_model.dart';
 
-abstract class AbstractClassRepository {
-  Future<List<ArticleModel>> getAllNews();
-  Future<List<ArticleModel>> getCategory(String category);
-}
