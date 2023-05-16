59d6b1b3c22ad3b642337bbd84d20ee690c8f58f
Wiam-ops <122654618+Wiam-ops@users.noreply.github.com>
2023-05-16 20:57:57 +0100

    Update Profil_screen.dart

--- lib/src/features/newsfeed/Pages/Profil_screen.dart
+++ lib/src/features/newsfeed/Pages/Profil_screen.dart
@@ -1,108 +1,85 @@
-/*import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 
-import '../repository/news_api.dart';
-import '../view_model/article_view_model.dart';
-import '../view_model/articles_view_model.dart';
-import 'article_details_screen.dart';
-
-class ProfilScreen extends StatefulWidget {
-  const ProfilScreen({super.key});
-
-  @override
-  State<ProfilScreen> createState() => _ProfilScreenState();
-}
-
-class _ProfilScreenState extends State<ProfilScreen> {
-  var articlesListViewModel = ArticlesListViewModel(classRepository: NewsApi());
-
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+  
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<ArticleViewModel>>(
-      future: articlesListViewModel.fetchNewsTechnology(),
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const CircularProgressIndicator();
-        } else {
-          var news = snapshot.data;
-          return Scaffold(
-            body: Swiper(
-              itemBuilder: (BuildContext context, int index) {
-                return InkWell(
-                  onTap: (() {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ArticleDetailsScreen(
-                          //  sourceName: news[index]!.sourceName,
-                          title: news[index]!.title,
-                          author: news[index]!.author,
-                          publishedAt: news[index]!.publishedAt,
-                          description: news[index]!.description,
-                          content: news[index]!.content,
-                          urlToImage: news[index]!.urlToImage,
-                          url: news[index]!.url,
-                        ),
-                      ),
-                    );
-                  }),
-                  child: Stack(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(10),
-                        child: ClipRRect(
-                          borderRadius: const BorderRadius.only(
-                            topLeft: Radius.circular(35.0),
-                            topRight: Radius.circular(35.0),
-                            bottomLeft: Radius.circular(35.0),
-                            bottomRight: Radius.circular(35.0),
-                          ),
-                          child: Image.network(
-                            news[index]!.urlToImage,
-                            fit: BoxFit.cover,
-                            height: 400.0,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 350.0, 0.0, 0),
-                        child: Container(
-                          height: 200.0,
-                          width: 750.0,
-                          child: Material(
-                            borderRadius: BorderRadius.circular(35.0),
-                            elevation: 10.0,
-                            child: Column(
-                              children: [
-                                Padding(
-                                  padding:
-                                      const EdgeInsets.fromLTRB(10, 20, 10, 20),
-                                  child: Text(
-                                    news[index].title,
-                                    style: const TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
+    var isDark =MediaQuery.of(context).platformBrightness==Brightness.dark;
+    
+    String tProfileHeading = "Nom complet";
+    String tProfileSubHeading = "Email institutionnel";
+    String tEditProfile='modifier Profile';
+    String tMenu='Se déconnecter ';
+    return Scaffold(
+      appBar: AppBar (
+        
+        
+        ),
+      body:SingleChildScrollView(
+        child : Container(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              SizedBox(
+                //width: 120,height: 120,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: Image.asset('assets/png_pf.png' ,width: 100,height: 100),
+                
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(tProfileHeading , style: Theme.of(context).textTheme.headline4,),
+              Text(tProfileSubHeading , style: Theme.of(context).textTheme.bodyText2,),
+              const SizedBox(height: 20),
+              SizedBox(
+                child: ElevatedButton(
+                  onPressed:() {},
+                  child:  Text(tEditProfile ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.yellow , side: BorderSide.none,shape: StadiumBorder()
+              )
+              )),
+              const SizedBox(height: 30),
+              const Divider(),
+              const SizedBox(height: 10),
+               //Menu 
+              ListTile(
+                leading: Container(
+                  width: 30,
+                  height: 30,
+                  decoration:BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    color: Colors.black.withOpacity(0.1),
                   ),
-                );
-              },
-              itemCount: news == null ? 0 : news!.length,
-              viewportFraction: 0.7,
-              scale: 0.9,
-              autoplay: true,
+                  child: Icon(LineAwesomeIcons.alternate_sign_out, color: Colors.yellow),
+                ),
+              
+                title: Text(tMenu , style: Theme.of(context).textTheme.bodyText1,),
+                trailing:Container(
+                  
+                  width: 30,
+                  height: 30,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    color: Colors.grey.withOpacity(0.1),
+                  ),
+                  child:IconButton(onPressed : (){}, icon : const Icon(LineAwesomeIcons.angle_right)),
+                  // child: const Icon(LineAwesomeIcons.angle_right,size: 18.0,color: Colors.grey )),
+                  
+                ))
+              
+            ],
             ),
-          );
-        }
-      },
+        ),
+        ),
     );
+  }
+  }
+
+
   }
 }
 */
